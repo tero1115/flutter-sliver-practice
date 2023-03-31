3e8a4cea48e32d99fc7008d5f486b9654e9ba1c0
tero1115 <limjisang1115@gmail.com>
2023-03-31 11:38:57 +0900

    슬리버 연습

---
+++ lib/ad.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Ad extends StatelessWidget {
+  final int index;
+  const Ad(this.index,{Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.lightBlue[100 * (index % 9)],
+      child: Text("Advertise $index"),
+    );
+  }
+}
